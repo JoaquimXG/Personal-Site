f2a4514a99328e205bf04b38a46d16a8b544b5db
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-10-17 12:07:34 +0100

    removing fonts

--- static.config.js
+++ static.config.js
@@ -6,6 +6,30 @@
 // Get started at https://react-static.js.org
 
 export default {
-  maxThreads: 1, // Remove this when you start doing any static generation
-	plugins: ["react-static-plugin-styled-components"],
-}
+    maxThreads: 1, // Remove this when you start doing any static generation
+
+    plugins: ["react-static-plugin-styled-components"],
+
+    Document: ({
+        Html,
+        Head,
+        Body,
+        children,
+        state: { siteData, renderMeta }
+    }) => (
+        <Html lang="en-US">
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Body>{children}</Body>
+        </Html>
+    )
+};
