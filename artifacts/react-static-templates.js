4d0aaa3ff9526711b4a99e91471a1282ac460514
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-01-01 13:30:44 +0000

    updating readme

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,12 +1,35 @@
 
 
+import React from 'react'
+import universal, { setHasBabelPlugin } from 'react-universal-component'
+
+setHasBabelPlugin()
+
+const universalOptions = {
+  loading: () => null,
+  error: props => {
+    console.error(props.error);
+    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
+  },
+  ignoreBabelRename: true
+}
+
+const t_0 = universal(import('__react_static_root__/node_modules/react-static/lib/browser/components/Default404'), universalOptions)
+      t_0.template = '__react_static_root__/node_modules/react-static/lib/browser/components/Default404'
+      
+const t_1 = universal(import('__react_static_root__/src/index'), universalOptions)
+      t_1.template = '__react_static_root__/src/index'
+      
+const t_2 = universal(import('__react_static_root__/src/components/NotFound'), universalOptions)
+      t_2.template = '__react_static_root__/src/components/NotFound'
+      
 
 // Template Map
 export default {
-  '__react_static_root__/../../../../../usr/lib/node_modules/react-static/lib/browser/components/Default404': require('__react_static_root__/../../../../../usr/lib/node_modules/react-static/lib/browser/components/Default404').default,
-'__react_static_root__/src/index': require('__react_static_root__/src/index').default,
-'__react_static_root__/src/components/NotFound': require('__react_static_root__/src/components/NotFound').default
+  '__react_static_root__/node_modules/react-static/lib/browser/components/Default404': t_0,
+'__react_static_root__/src/index': t_1,
+'__react_static_root__/src/components/NotFound': t_2
 }
-
-export const notFoundTemplate = '__react_static_root__/../../../../../usr/lib/node_modules/react-static/lib/browser/components/Default404'
+// Not Found Template
+export const notFoundTemplate = "__react_static_root__/node_modules/react-static/lib/browser/components/Default404"
 
