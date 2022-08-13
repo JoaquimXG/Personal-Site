923322b40edb569f33799c1d280e5573b0da52e4
Joaquim Gomez <joaquim.gomez@bbconsult.co.uk>
2022-08-13 17:17:12 +0100

    Shrinking footer size for smaller screens

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,10 +2,10 @@
   footer
     margin: 0 0 64px
     text-align: center
-    font-size: 1.6rem
+    font-size: 1.4rem
 
   a
-    font-size: 1.6rem
+    font-size: 1.4rem
 </style>
 
 <footer>Built with ❤ with <a class="underline" href="https://elderguide.com/tech/elderjs/">Elder.js</a></footer>
